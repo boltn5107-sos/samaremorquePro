6923a00d4df7747a7c8cd1ae17152f7640457c3d
boltn5107-sos <boltn5012@gmai.com>
2026-09-19 07:00:33 +0000

    Nettoyage: ignorer les fichiers runtime de storage (sessions, vues compiles, logs)

--- storage/framework/views/f6f1b10d155bf35877b3eed58c1b3e57.php
+++
@@ -1,5 +0,0 @@
-<thead class="sticky z-10 top-0 bg-white dark:bg-gray-900">
-    <?php echo e($slot); ?>
-
-</thead>
-<?php /**PATH C:\xampp\htdocs\samaRemorque\senegal-towing\vendor\laravel\pulse\resources\views\components\thead.blade.php ENDPATH**/ ?>
